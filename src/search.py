c21bee7c1bdca67900d8efc14282a2b3213b2d03
XuShuo <xushuo@highlight.mobi>
2022-02-07 20:04:19 +0800

    分数爬取完成

--- src/search.py
+++ src/search.py
@@ -1,41 +1,24 @@
 import requests
 import urllib
+import string_proc
 import json
 
 
 def search(apps):
     file_name = '../response_app_id/app_id.txt'
     log = open(file_name, 'w')
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 ',
-        'authority': 'mollusk.apis.ign.com',
-        'method': 'GET',
-        'path': '/graphql?operationName=SearchObjectsByName&variables=%7B%22term%22%3A%22456%22%2C%22count%22%3A20%2C%22sortBy%22%3A%22updatedAt%22%2C%22sortOrder%22%3A%22desc%22%2C%22region%22%3A%22us%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22a44ef5a9532d087ac58e59c5c6a4d3f1f219550c88c86d3b16998973f293f123%22%7D%7D',
-        'scheme': 'https',
-        'accept': '* / *',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'apollographql-client-name': 'kraken'
-    }
     for app in apps:
-        variables = urllib.parse.quote(app[1])
-        params = {
-            "operationName": "SearchObjectsByName",
-            "variables": variables,
-            "extensions": '%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22a44ef5a9532d087ac58e59c5c6a4d3f1f219550c88c86d3b16998973f293f123%22%7D%7D'
+        slug = string_proc.slug_proc(app[2])
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 '
         }
-        # response = requests.get(headers=headers, params=params, url='https://mollusk.apis.ign.com/graphql')
-        url = 'https://mollusk.apis.ign.com/graphql?operationName=SearchObjectsByName&variables=%7B%22term%22%3A%22' + variables + '%22%2C%22count%22%3A20%2C%22sortBy%22%3A%22updatedAt%22%2C%22sortOrder%22%3A%22desc%22%2C%22region%22%3A%22us%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22a44ef5a9532d087ac58e59c5c6a4d3f1f219550c88c86d3b16998973f293f123%22%7D%7D'
-        response = requests.get(headers=headers,
-                                url=url)
-        if response is None:
-            continue
-        response_json = response.json()
-        if len(response_json['data']['searchObjectsByName']['objects']) > 0:
-            log.write(str(app[0]) + ' ' + app[1] + ' ' + app[2])
-            print(str(app[0]) + ' ' + app[1] + ' ' + app[2] + ' Successfully')
+        response = requests.get("https://www.ign.com/games/"+slug, headers=headers)
+        rating=string_proc.find_value(str(response.content))
+        if rating!=-1:
+            log.write(str(app[0]) + ' ' + app[1] + ' ' + app[2] + str(rating))
+            print(str(app[0]) + ' ' + app[1] + ' ' + app[2] + ' Successfully!!! Rating Value = '+str(rating))
             log.write('\n')
             log.flush()
         else:
-            print(str(app[0]) + ' ' + app[1] + ' ' + app[2] + ' Failed')
+            print(str(app[0]) + ' ' + app[1] + ' ' + app[2]+'No found rating')
     log.close()
---
+++ src/string_proc.py
@@ -0,0 +1,28 @@
+import string
+
+
+def slug_proc(slug: string):
+    if slug.endswith('-mod-apk'):
+        slug = slug.replace('-mod-apk', '')
+    elif slug.endswith('-mod'):
+        slug = slug.replace('-mod', '')
+    elif slug.endswith('-apk-mod'):
+        slug = slug.replace('-apk-mod', '')
+    elif slug.endswith('-apk'):
+        slug = slug.replace('-apk', '')
+    return slug
+
+
+def find_value(source):
+    value_str=""
+    begin_index = source.find('ratingValue') + 13
+    end_index = source.find(',', begin_index) - 1
+    if begin_index!=12:
+        i = begin_index
+        while i<=end_index:
+            value_str+=source[i]
+            i+=1
+    if value_str== "":
+        return -1
+    return float(value_str)
+
